d9d4b277b24add6eaef28251a1a0855ddbfd69a2
Jamesh431 <james.devprojects@gmail.com>
2024-12-14 18:33:08 -0700

    created testing ping pong command

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-import { Client } from "discord.js";
+import { Client, Events, SlashCommandBuilder } from "discord.js";
 import { config } from "dotenv";
 
 const client = new Client({intents: []});
 const noosphere = config().parsed
 const token = noosphere.token
+
+
+client.once(Events.ClientReady, c => {
+  console.log(`${c.user.username} is ready to serve: `)
+
+  const ping = new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with pong!")
+
+  client.application.commands.create(ping);
+})
+
+client.on(Events.InteractionCreate, inter => {
+  // console.log(inter)
+
+  if (inter.commandName === "ping") {
+    inter.reply("Pong!")
+  }
+})
+
 
 
 client.login(token)
