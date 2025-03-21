f458e21fb4ed7116c758fc6810d12ba63c85430f
Jamesh431 <james.devprojects@gmail.com>
2025-03-21 16:52:30 -0600

    created googleInit()

---
+++ googleInit.js
@@ -0,0 +1,7 @@
+/*
+Initialize using google api here, then using Googles push notifications for emails push new emails to console. Remember to import into index and call initialization and push notifications
+*/
+
+export default function googleInit() {
+  // need to pull api key from config.json for gmail, and any other needed information
+}
