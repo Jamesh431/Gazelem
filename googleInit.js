7110c8bad185be4f599e895e9c7dcee7bcb76b83
Jamesh431 <james.devprojects@gmail.com>
2025-06-17 19:16:55 -0600

    initilializes google await function

--- googleInit.js
+++ googleInit.js
@@ -1,7 +1,34 @@
+import { google } from "googleapis";
+
 /*
 Initialize using google api here, then using Googles push notifications for emails push new emails to console. Remember to import into index and call initialization and push notifications
 */
 
-export default function googleInit() {
+const fetchData = await fetch('./client.json')
+const data = await fetchData.json()
+const web = data.web
+
+const googleAuth = new google.auth.OAuth2(web.client_id, web.client_secret, "urn:ietf:wg:oauth:2.0:oob")
+
+const accountGmail = google.gmail({ version: 'v1', auth });
+
+async function googleInit() {
   // need to pull api key from config.json for gmail, and any other needed information
+
+
+  googleAuth.setCredentials()
+
+  const res = await gmail.users.watch({
+    userId: 'me',
+    requestBody: {
+      labelIds: ['INBOX'], // this currently only notifies on new inbox messages
+      topicName: `projects/${web.project_id}}/topics/${web.topic_name}`,
+    },
+  });
+
+
+  console.log('Gmail Watch started:', res.data);
 }
+
+
+export {googleAuth, accountGmail, googleInit}
